c4829edd06c8d1fe9227d6e604a76e472092e4ac
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2022-11-07 15:42:47 +0200

    login page

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <input type="text" v-model="email" />
+  <input type="password" v-model="password" />
+  <button @click="signUp">signUp</button>
+</template>
+<script setup>
+const email = ref("");
+const password = ref("");
+const client = useSupabaseClient();
+const signUp = async () => {
+  const { user, error } = await client.auth.signUp({
+    email: email.value,
+    password: password.value,
+  });
+  console.log("user", user);
+  console.log("error", error);
+};
+</script>
+<style lang="scss"></style>
