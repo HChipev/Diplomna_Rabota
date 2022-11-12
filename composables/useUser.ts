af6f832c9fd554b7106075c713c94eb860a0c70e
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2022-11-12 22:41:36 +0200

    user management

--- composables/useUser.ts
+++ composables/useUser.ts
@@ -12,6 +12,7 @@ export const useUser = defineStore("user", {
       this.user = payload ? payload.user : null;
       if (this.user) {
         localStorage.setItem("user", JSON.stringify(payload.user));
+        this.user = JSON.parse(localStorage.getItem("user"));
       }
       // } else {
       //   localStorage.removeItem("user");
