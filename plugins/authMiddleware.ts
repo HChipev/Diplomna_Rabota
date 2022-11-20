87c30ad3b2e1d920ff6042a617bffb323b8aed31
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2022-11-20 17:38:06 +0200

    middleware

--- plugins/authMiddleware.ts
+++ plugins/authMiddleware.ts
@@ -2,11 +2,9 @@ export default defineNuxtPlugin(() => {
   addRouteMiddleware(
     (to) => {
       if (useUser().getUser && to.path === "/login") {
-        console.log("user", useUser().getUser);
-        return navigateTo({ path: "/account" });
+        return navigateTo("/account");
       } else if (!useUser().getUser && to.path === "/account") {
-        console.log("no user", useUser().getUser);
-        return navigateTo({ path: "/login" });
+        return navigateTo("/login");
       }
     },
     { global: true }
