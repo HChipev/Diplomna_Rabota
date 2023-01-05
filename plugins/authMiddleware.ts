278aba9079a7ea09bc1ac6df7696db9c25395e47
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-01-05 14:55:47 +0200

    state management

--- plugins/authMiddleware.ts
+++ plugins/authMiddleware.ts
@@ -1,11 +1,11 @@
 export default defineNuxtPlugin(() => {
   addRouteMiddleware(
-    async (to) => {
-      await useUser().requestUser();
+    (to) => {
+      useUser().requestUser();
       console.log(useUser().getUser);
-      if (useUser().getUser && to.path === "/login") {
+      if (useSupabaseUser() && to.path === "/login") {
         return navigateTo("/account");
-      } else if (!useUser().getUser && to.path === "/account") {
+      } else if (!useSupabaseUser() && to.path === "/account") {
         return navigateTo("/login");
       }
     },
