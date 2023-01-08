40789042ca558d9598827162a77df60d626d556f
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-01-08 21:45:25 +0200

    database

--- composables/useSupabaseUserDB.js
+++ composables/useSupabaseUserDB.js
@@ -1,18 +1,22 @@
 export async function updateUser(_id, _role, _phone) {
-  const { data, error } = await useSupabaseClient()
-    .from("user")
-    .insert([{ user_id: _id, role: _role, phone: _phone }]);
-  console.log(useUser().getUser.id);
-  console.log(error);
+  if (process.client) {
+    const { data, error } = await useSupabaseClient()
+      .from("user")
+      .insert([{ user_id: _id, role: _role, phone: _phone }]);
+    console.log(useUser().getUser.id);
+    console.log(error);
+  }
 }
 //*for getting data from supabase
 export async function getPhone() {
-  let { data: user, error } = await useSupabaseClient()
-    .from("user")
-    .select("phone");
-  if (user.length > 0) {
-    console.log(user[0].phone);
-    return user[0].phone;
+  if (process.client) {
+    let { data: user, error } = await useSupabaseClient()
+      .from("user")
+      .select("phone");
+    if (user.length > 0) {
+      console.log(user[0].phone);
+      return user[0].phone;
+    }
+    return "";
   }
-  return "";
 }
