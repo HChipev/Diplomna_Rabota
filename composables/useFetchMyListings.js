e6644bf4aa3cb5531fd73a5f5ad5237406c56c3d
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-01-22 16:44:15 +0200

    testing 500 error

--- composables/useFetchMyListings.js
+++ composables/useFetchMyListings.js
@@ -1,5 +1,7 @@
 export default async function useFetchMyListings(user_id) {
   const { data, error } = await useFetch(`/api/car/listings/user/${user_id}`);
+  console.log("data", data);
+  console.log("error", error);
   if (error.value) {
     throwError({ ...error.value, message: "Error fetching my listings" });
   }
