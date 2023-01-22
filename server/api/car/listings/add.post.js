785fb323e7d8941a912a2da81c23963fa3deebd9
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-01-22 10:54:33 +0200

    fixed database

---
+++ server/api/car/listings/add.post.js
@@ -0,0 +1,77 @@
+import Joi from "joi";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+const schema = Joi.object({
+  makeId: Joi.number().integer().required(),
+  modelId: Joi.number().integer().required(),
+  engineId: Joi.number().integer().required(),
+  gearboxId: Joi.number().integer().required(),
+  drivetrainId: Joi.number().integer().required(),
+  regionId: Joi.number().integer().required(),
+  cityId: Joi.number().integer().required(),
+  colorId: Joi.number().integer(),
+  year: Joi.number()
+    .integer()
+    .min(1930)
+    .max(new Date().getFullYear())
+    .required(),
+  price: Joi.number().integer().min(0).required(),
+  horsepower: Joi.number().integer().min(0).required(),
+  mileage: Joi.number().integer().min(0).required(),
+  features: Joi.array().items(Joi.string()),
+  description: Joi.string(),
+  isOnParts: Joi.boolean().required(),
+  userId: Joi.string().required(),
+});
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const { error, value } = schema.validate(body);
+  if (error) {
+    throw createError({
+      statusCode: 412,
+      statusMessage: error.message,
+    });
+  }
+  const {
+    makeId,
+    modelId,
+    engineId,
+    gearboxId,
+    drivetrainId,
+    regionId,
+    cityId,
+    colorId,
+    year,
+    price,
+    horsepower,
+    mileage,
+    features,
+    description,
+    isOnParts,
+    userId,
+  } = value;
+
+  const car = await prisma.Car.create({
+    data: {
+      makeId,
+      modelId,
+      engineId,
+      gearboxId,
+      drivetrainId,
+      regionId,
+      cityId,
+      colorId,
+      year,
+      price,
+      horsepower,
+      mileage,
+      features,
+      description,
+      isOnParts,
+      userId,
+    },
+  });
+
+  return car;
+});
