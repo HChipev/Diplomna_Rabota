b50d3837ffb55f9c337e658839e78e05aacd9fa6
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-02-07 13:12:08 +0200

    fixing saved car on delete

--- pages/account/cart/index.vue
+++ pages/account/cart/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Text Cart</div>
+  <div class="container mx-auto flex justify-center items-center">
+    <div
+      class="flex flex-col justify-between rounded-md shadow-sm w-[75%] shadow-white p-3">
+      <h1 class="text-2xl">Cart items</h1>
+      <div
+        class="flex flex-col m-2 p-2 rounded-lg border border-border-color"></div>
+    </div>
+  </div>
 </template>
 <script setup>
   useHead({
--- server/api/car/listings/[listingId].delete.js
+++ server/api/car/listings/[listingId].delete.js
@@ -2,10 +2,37 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export default defineEventHandler(async (event) => {
   const { listingId } = event.context.params;
-
-  return await prisma.car.delete({
+  const savedCars = await prisma.user.findMany({
+    where: {
+      savedCarsId: {
+        has: parseInt(listingId),
+      },
+    },
+    select: {
+      savedCarsId: true,
+      id: true,
+    },
+  });
+  let removeSaved;
+  for (let i = 0; i < savedCars.length; i++) {
+    console.log(savedCars[i].id);
+    removeSaved = await prisma.user.update({
+      where: {
+        id: savedCars[i].id,
+      },
+      data: {
+        savedCarsId: {
+          set: savedCars[i].savedCarsId.filter(
+            (id) => id !== parseInt(listingId)
+          ),
+        },
+      },
+    });
+  }
+  const deletedCar = await prisma.car.delete({
     where: {
       id: parseInt(listingId),
     },
   });
+  return { deletedCar, removeSaved };
 });
